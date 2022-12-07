dd15ab1021494a64b49b1eb15c2d55aa191595a2
AdonNeet <baradontap@gmail.com>
2022-12-07 11:05:50 +0700

    Update tryFunction_L200220194.py
    Bara Donta Perdana
    L200220194
    
    Mencoba menggunakan function
    
    v1.0

--- Experiment/tryFunction_L200220194.py
+++ Experiment/tryFunction_L200220194.py
@@ -1,3 +1,14 @@
-print("Hello World")
+def penjumlahan(x):
+    return x
 
-print('Ngetes nge push file ke branch experiment')
+ans = 0
+
+print('Jika angka 0 dimasukkan, maka perulangan input akan berhenti')
+while True:
+    num = int(input('Masukkan angka yang ingin ditambahkan: '))
+    if(num != 0):
+       ans += penjumlahan(int(num))
+    else:
+        break
+
+print('Hasil penjumlahannya yaitu', ans)
