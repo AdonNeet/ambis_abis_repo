e0a236a0a1c6f8239ed39717ca6697c6d6fb1da7
MolyXx <ardhanaulia222@gmail.com>
2022-12-12 15:38:19 +0700

    Create tryFunction_L200220096.py
    Muhammad Ardhan Aulia Rahman
    L200220096
    
    Mencoba fungsi
    
    v1.0

---
+++ Experiment/tryFunction_L200220096.py
@@ -0,0 +1,16 @@
+def negara(country = "Indonesia"):
+  print("Saya dari " + country)
+
+negara()
+negara("Portugal")
+negara("France")
+negara("Brazil")
+
+print("-----------------------------------------------------")
+
+def jumlah(num1, num2):
+    dat = num1 + num2
+    print("Hasil ",dat)
+
+jumlah(5, 4)
+jumlah(10, 10)
